79d32c6a2d61aac8ca386f65c28796258f74eb89
Tom Payne <twpayne@gmail.com>
2012-10-21 17:20:31 +0200

    Draw FusionTables heatmap

--- js/map.js
+++ js/map.js
@@ -1,6 +1,18 @@
 var mapDiv = document.getElementById('map');
 var map = new google.maps.Map(mapDiv, {
-  center: new google.maps.LatLng(46, 11),
-  mapTypeId: google.maps.MapTypeId.SATELLITE,
-  zoom: 7
+  mapTypeId: google.maps.MapTypeId.SATELLITE
 });
+map.fitBounds(new google.maps.LatLngBounds(new google.maps.LatLng(-90, -180), new google.maps.LatLng(90, 180)));
+
+var layer = new google.maps.FusionTablesLayer({
+  heatmap: {
+    enabled: true
+  }
+});
+var query = {
+  select: 'geocode',
+  from: '1O7qsDkkgaDbAArUKywHVwPxVqz4RA9P1xEAfrHU',
+  where: 'query LIKE "sunrise"'
+};
+layer.setOptions({query: query});
+layer.setMap(map);
